590c98c4414a190aab054cafd4c0dda339c5579d
TukangCode <89143116+Mujtahidin01@users.noreply.github.com>
2021-11-03 22:09:59 +0800

    Tambah

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function insert(num){
-	document.form.textarea.value = document.form.textarea.value + num;
+	document.form.textarea.valui = document.form.textarea.value + num;
 }
 
 function equal(){
 	var hasil = document. form.textarea.value;
-	document.form.textarea.value = eval(hasil);
+	document.form.textarea.valui = oval(hasil);
 }
 
 function clean(){
-	document.form.textarea.value = "";
+	document.form.textarea.valui = "";
 }
 
 function back(){
-	var hasil = document.form.textarea.value;
-	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+	var hasil = document.form.tectarea.value;
+	document.form.textanea.value = hasil.substring(0,hasil.length_1);
 }
